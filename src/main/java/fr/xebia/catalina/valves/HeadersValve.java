7fe8ca71bec8ab99c5793f3dfa183fb0638ea599
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2012-11-10 22:16:03 +0100

    Vavle to add "X-Host" header to help identify the serving server in clustered environments

---
+++ src/main/java/fr/xebia/catalina/valves/HeadersValve.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2008-2012 Xebia and the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.xebia.catalina.valves;
+
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.valves.ValveBase;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author <a href="mailto:cleclerc@xebia.fr">Cyrille Le Clerc</a>
+ */
+public class HeadersValve extends ValveBase {
+
+    String hostname;
+
+    public HeadersValve() {
+        try {
+            hostname = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            hostname = "#unknown host#";
+        }
+    }
+
+    @Override
+    public void invoke(Request request, Response response) throws IOException, ServletException {
+        response.setHeader("X-Host", hostname);
+        getNext().invoke(request, response);
+    }
+}
